56f57ff4e9c7675e849f7f8bf1ba6cc46a0be3b7
CatiaAntunes <catiafantunes@gmail.com>
2023-03-19 16:20:10 +0000

    Delete database_inserts.sql

--- database_inserts.sql
+++
@@ -1,6 +0,0 @@
-use todos;
-insert into Todo (id, title, `desc`) values 
-(1, "Take over the galaxy", "MWAHAHAHAHAHAHAHAHAHAHAHAHAMWAHAHAHAHAHAHAHAHAHAHAHAHA"),
-(2, "destroy alderaan", "Make sure there are no survivors left!"),
-(3, "play space invaders", "Become the very best!"),
-(4, "crush rebel scum", "Every.Last.One.");
